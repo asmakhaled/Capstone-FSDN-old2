1ec1a917be9e9f8495e85665c1c8ba5e5790b304
Asma Khaled <46532396+asmakhaled@users.noreply.github.com>
2020-12-12 11:22:54 +0300

    Update auth.py

--- starter/auth.py
+++ starter/auth.py
@@ -4,10 +4,10 @@ from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
 
+AUTH0_DOMAIN = os.environ['AUTH0_DOMAIN']
+ALGORITHMS = os.environ['ALGORITHMS']
+API_AUDIENCE = os.environ['API_AUDIENCE']
 
-AUTH0_DOMAIN = 'asma-khaled.eu.auth0.com'
-ALGORITHMS = ['RS256']
-API_AUDIENCE = 'agency-api'
 
 # AuthError Exception
 '''
